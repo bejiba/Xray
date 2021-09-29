25c69a28efc42724598e01b84fa65d92c5a1fff3
frainzy1477 <49537471+frainzy1477@users.noreply.github.com>
2021-09-29 18:19:18 +0000

    0.7.2.0

---
+++ api/apimodel.go
@@ -0,0 +1,79 @@
+package api
+
+
+// API config
+type Config struct {
+	APIHost      string  `mapstructure:"ApiHost"`
+	NodeID       int     `mapstructure:"NodeID"`
+	Key          string  `mapstructure:"ApiKey"`
+	Timeout      int     `mapstructure:"Timeout"`
+	SpeedLimit   float64 `mapstructure:"SpeedLimit"`
+	DeviceLimit  int     `mapstructure:"DeviceLimit"`
+	RuleListPath string  `mapstructure:"RuleListPath"`
+}
+
+// Node status
+type NodeStatus struct {
+	CPU    float64
+	Mem    float64
+	Disk   float64
+	Uptime int
+}
+
+type NodeInfo struct {
+	NodeType          string // Must be V2ray, Trojan, and Shadowsocks
+	NodeID            int
+	Port              int
+	SpeedLimit        uint64 // Bps
+	AlterID           int
+	TransportProtocol string
+	Host              string
+	Path              string
+	EnableTLS         bool
+	TLSType           string
+	EnableVless       bool
+	CypherMethod      string
+	ServiceName       string
+	HeaderType        string
+}
+
+
+type UserInfo struct {
+	UID           int
+	Email         string
+	Passwd        string
+	Port          int
+	SpeedLimit    uint64 
+	DeviceLimit   int
+	UUID          string
+	AlterID       int
+	IPcount       int
+}
+
+type OnlineUser struct {
+	UID int
+	IP  string
+}
+
+type UserTraffic struct {
+	UID      int
+	Email    string
+	Upload   int64
+	Download int64
+}
+
+type ClientInfo struct {
+	APIHost  string
+	NodeID   int
+	Key      string
+}
+
+type DetectRule struct {
+	ID      int
+	Pattern string
+}
+
+type DetectResult struct {
+	UID    int
+	RuleID int
+}
---
+++ api/xmanager/model.go
@@ -0,0 +1,71 @@
+package xmanager
+
+import "encoding/json"
+
+// NodeInfoResponse is the response of node
+type NodeInfoResponse struct {
+	Group           int     `json:"node_group"`
+	Class           int     `json:"node_class"`
+	SpeedLimit      float64  `json:"node_speedlimit"`
+	TrafficRate     float64 `json:"traffic_rate"`
+	Method		    string  `json:"method"`
+	Port		    int     `json:"port"`
+	RawServerString string  `json:"server"`
+	Type            string  `json:"type"`
+	EnableVless		bool	`json:"vless"`
+	EnableXTLS		bool	`json:"xtls"`   
+}
+
+
+// UserResponse is the response of user
+type UserResponse struct {
+	ID            int    `json:"id"`
+	Email         string `json:"email"`
+	Passwd        string `json:"passwd"`
+	SpeedLimit    float64 `json:"node_speedlimit"`
+	DeviceLimit   int    `json:"node_connector"`
+	ForbiddenIP   string `json:"forbidden_ip"`
+	ForbiddenPort string `json:"forbidden_port"`
+	UUID          string `json:"uuid"`
+	IPcount       int    `json:"online_ip_count"`
+}
+
+// Response is the common response
+type Response struct {
+	Ret  uint            `json:"ret"`
+	Data json.RawMessage `json:"data"`
+}
+
+// PostData is the data structure of post data
+type PostData struct {
+	Data interface{} `json:"data"`
+}
+
+// SystemLoad is the data structure of systemload
+type SystemLoad struct {
+	Uptime string `json:"uptime"`
+	Load   string `json:"load"`
+}
+
+// OnlineUser is the data structure of online user
+type OnlineUser struct {
+	UID int    `json:"user_id"`
+	IP  string `json:"ip"`
+}
+
+// UserTraffic is the data structure of traffic
+type UserTraffic struct {
+	UID      int   `json:"user_id"`
+	Upload   int64 `json:"u"`
+	Download int64 `json:"d"`
+}
+
+type RuleItem struct {
+	ID      int    `json:"id"`
+	Content string `json:"regex"`
+}
+
+type IllegalItem struct {
+	ID  int `json:"list_id"`
+	UID int `json:"user_id"`
+}
