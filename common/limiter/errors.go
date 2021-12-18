8893e17b957cc523a8e92aeda1791d35a5133333
frainzy1477 <frainzy1477@protonmail.com>
2021-12-18 22:43:03 +0000

    dev

---
+++ common/limiter/errors.go
@@ -0,0 +1,9 @@
+package limiter
+
+import "github.com/xcode75/xray-core/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}
